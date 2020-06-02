6a212078d022714d2e61895ec577ab23e803c25e
tt <tamara.trefilova@gmail.com>
2020-06-01 21:14:37 -0700

    changes fro pom

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,8 +1,57 @@
+import java.util.Arrays;
 
 public class NewClass {
 
 
-		public String newvalue ="string";
+		public String newValue ="string";
 	
+		
+		 public String getNewValue() {
+			return newValue;
+		}
+
+
+		public void setNewValue(String newValue) {
+			this.newValue = newValue;
+		}
+
+
+		public static void main(String[] args) {
+			 
+			
+//			NewClass value = new NewClass() ;
+//			 
+//			 System.out.println(value.getNewValue());
+			 
+			 
+			    // 1. Initialize
+		        int[] a0 = new int[5];
+		        int[] a1 = {3, 2, 4};
+		        // 2. Get Length
+		        System.out.println("The size of a1 is: " + a1.length);
+		        // 3. Access Element
+		        System.out.println("The first element is: " + a1[0]);
+		        // 4. Iterate all Elements
+		        System.out.print("[Version 1] The contents of a1 are:");
+		        for (int i = 0; i < a1.length; ++i) {
+		            System.out.print(" " + a1[i]);
+		        }
+		        System.out.println();
+		        System.out.print("[Version 2] The contents of a1 are:");
+		        for (int item: a1) {
+		            System.out.print(" " + item);
+		        }
+		        System.out.println();
+		        // 5. Modify Element
+		        a1[0] = 5;
+		        // 6. Sort
+		        Arrays.sort(a1);
+		        System.out.print("[Version 3] The contents of a1 are:");
+		        for (int item: a1) {
+		            System.out.print(" " + item);
+		        }
+		    
+			
+		 }
 
 }
